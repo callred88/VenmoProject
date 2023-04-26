625eb1ba1b402b5c63fe3f5ffee699d78bccfa93
callred88 <callred88@outlook.com>
2023-04-25 20:53:54 -0500

    I have almost completed the project

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ user_one = {
     "full_name": "Sheldon Cooper",
     "user_name": "Bazinga",
     "password": "Stringtheory",
-    "account_balance": "5000",
+    "account_balance": 5000,
     "connected_banks": [
-        ("bank-one", {"bank_name": "Chase", "available_balance": 10000}),
-        ("bank-two", {"bank_name": "Discover", "available_balance": 7500})
+        ("Chase",10000),
+        ("Discover",7500)
     ]
 }
 
@@ -13,10 +13,10 @@ user_two = {
     "full_name": "Howard Wolowitz",
     "user_name": "Engineer",
     "password": "Nasamission",
-    "account_balance": "950",
+    "account_balance": 950,
     "connected_banks": [
-        ("bank-one", {"bank_name": "Regions", "available_balance": 4500}),
-        ("bank-two", {"bank_name": "Citigroup", "available_balance": 15450})
+        ("Regions",4500),
+        ("Citigroup",15450)
     ]
 }
 
@@ -33,11 +33,25 @@ def bank_transaction ():
     print("This is your current information on your account........")
     print("Your available balance is ",(user_one.get("account_balance")))
     print("The funds available in the connect accounts are:")
-    for bank,balance in user_one:
-     bank = user_one["connected_banks"][0][1]["bank_name"]
-     balance = user_one["connected_banks"][0][1]["available_balance"]
-     print(bank,":","$",balance,"available" )
-    
+    for bank in user_one["connected_banks"]:
+      bank_name ,balance = bank
+      print(f"{bank_name}: $ {balance} ")
+    name = user_two["full_name"]
+    transfer = input(f"Would you like to transfer money to {name}?")
+    if transfer != "yes":
+        transfer = input(f"Would you like to transfer money to {name}?")
+    if transfer == "yes":
+        transfer_amount = input("How much would you like to transfer?")
+        money = int(transfer_amount)
+    if money > user_one["account_balance"] :
+        print("You do not have enough funds.")
+        transfer_amount = input("How much would you like to transfer?")
+    if money < user_one["account_balance"] :
+        money_after_transfer = user_one["account_balance"] - money
+        user_two["account_balance"]= user_two["account_balance"] + money
+        transfer = input(f"Would you like to transfer money to {name}?")
+        
+        
        
 
     
